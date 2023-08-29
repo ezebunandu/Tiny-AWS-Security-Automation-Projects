b25ec3fa0cde6d6264c6123c6c82f47842fdc738
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2023-08-29 16:34:47 -0600

    refactored lambda function

--- root_user_access_key/index.py
+++ root_user_access_key/index.py
@@ -1,11 +1,41 @@
 import boto3
 import json
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+SNS_TOPIC_NAME = "RootUserAccessKeyTopic"
+
+
+def get_sns_topic_arn(name):
+    # If you attempt to create an already existing topic
+    # it will just return an object representing that topic
+    # https://stackoverflow.com/questions/71088521/how-can-i-get-the-sns-topic-arn-using-the-topic-name-to-publish-a-sns-message-in
+    sns_client = boto3.client("sns")
+    topic = sns_client.create_topic(Name=name)
+    return topic["TopicArn"]
+
+
+def notify_admin(topic):
+    sns_client = boto3.client("sns")
+    response = sns_client.publish(
+        TargetArn=topic,
+        Subject="Alert: Root User Access Key Creation Detected",
+        Message="An access key was created for the root user. Please investigate immediately",
+    )
+
+    if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+        logger.info(f"SNS notification sent successfully - {response}")
+    else:
+        logger.error(f"Error sending SNS notification - {response}")
+
+    return response
 
 
 def lambda_handler(event, context):
-    # Initialize the AWS SDK clients
-    sns_client = boto3.client("sns")
-    # Check if the event contains "CreateAccessKey" API event for the root user
+    # Verify that the event contains "CreateAccessKey" API event for the root user
     try:
         detail = event["detail"]
 
@@ -13,24 +43,13 @@ def lambda_handler(event, context):
             detail["eventName"] == "CreateAccessKey"
             and detail["userIdentity"]["type"] == "Root"
         ):
-            print("Access Key was created for the root user!")
-
-            email_subject = (
-                "Alert: Root User Access Key Creation Detected and Access Key Deleted"
-            )
-            email_body = "An access key was created for the root user. Please investigate immediately."
-
-            # https://stackoverflow.com/questions/71088521/how-can-i-get-the-sns-topic-arn-using-the-topic-name-to-publish-a-sns-message-in
-            topic = sns_client.create_topic(Name="RootUserAccessKeyTopic")
-            topic_arn = topic["TopicArn"]
-            sns_client.publish(
-                TopicArn=topic_arn,
-                Message=email_body,
-                Subject=email_subject,
-            )
+            logger.info("An access key was created for the root user")
+            topic = get_sns_topic_arn(SNS_TOPIC_NAME)
+            notify_admin(topic=topic)
+            logger.info("Notified the admin")
 
     except Exception as e:
-        print(f"Error processing event record: {e}")
+        logger.error(f"{e}")
 
     return {
         "statusCode": 200,
