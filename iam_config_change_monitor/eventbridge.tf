9bb6e645d846516e121785acacb524bbbbbfbc53
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2023-09-03 23:38:14 -0600

    eventbridge executes a lambda that publishes to sns

--- iam_config_change_monitor/eventbridge.tf
+++ iam_config_change_monitor/eventbridge.tf
@@ -24,14 +24,88 @@ resource "aws_cloudwatch_event_rule" "iam-config-change-monitor" {
   })
 }
 
-resource "aws_cloudwatch_event_target" "sns" {
+resource "aws_cloudwatch_event_target" "lambda_event_target" {
   rule      = aws_cloudwatch_event_rule.iam-config-change-monitor.name
-  target_id = "SendToSNS"
-  arn       = aws_sns_topic.iam-config-change-monitor.arn
+  arn       = aws_lambda_function.iam_config_change_monitor.arn
+  target_id = "iam-config-change-monitor"
+
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-role-for-function-execution"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach the required policy to the Lambda role
+resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
+  name       = "lambda_role_attachment"
+  policy_arn = "arn:aws:iam::aws:policy/AWSLambdaExecute" # Built-in AWS managed policy for Lambda execution
+  roles      = [aws_iam_role.lambda_role.name]
+}
+
+resource "aws_lambda_function" "iam_config_change_monitor" {
+  filename         = "lambda_function.zip"
+  function_name    = "iam_config_change_notifier"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "iam_config_monitor.lambda_handler"
+  runtime          = "python3.8"
+  source_code_hash = filebase64sha256("lambda_function.zip")
+}
+
+resource "aws_iam_policy" "publish-to-sns" {
+  name = "lambda-publish-to-sns"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "sns:Publish"
+        ],
+        Resource = aws_sns_topic.iam-config-change-monitor.arn
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "sns:CreateTopic"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the IAM policy for sns to the Lambda role
+resource "aws_iam_role_policy_attachment" "lambda_sns_publish_attachment" {
+  policy_arn = aws_iam_policy.publish-to-sns.arn
+  role       = aws_iam_role.lambda_role.name
+}
+
+# Allows the cloudwatch event be able to trigger the Lambda function
+resource "aws_lambda_permission" "event_rule_permission" {
+  statement_id  = "AllowExecutionFromCloudWatchEvent"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.iam_config_change_monitor.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.iam-config-change-monitor.arn
 }
 
 resource "aws_sns_topic" "iam-config-change-monitor" {
-  name = "iam-config-change-monitor-topic"
+  name         = "iam-config-change-monitor-topic"
+  display_name = "Receive Alerts When an API call is made to change the IAM config of the account"
 }
 
 resource "aws_sns_topic_subscription" "email_subscription" {
@@ -39,24 +113,3 @@ resource "aws_sns_topic_subscription" "email_subscription" {
   protocol  = "email"
   endpoint  = "samuel.ezebunandu@outlook.com" # change this to the email you want to subscribe to the topic
 }
-
-resource "aws_sns_topic_policy" "cloudwatch-event" {
-  arn    = aws_sns_topic.iam-config-change-monitor.arn
-  policy = data.aws_iam_policy_document.sns_topic_policy.json
-}
-
-data "aws_iam_policy_document" "sns_topic_policy" {
-  statement {
-    effect  = "Allow"
-    actions = ["SNS:Publish"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["events.amazonaws.com"]
-    }
-
-    resources = [aws_sns_topic.iam-config-change-monitor.arn]
-  }
-}
-
-
---
+++ iam_config_change_monitor/iam_config_monitor.py
@@ -0,0 +1,54 @@
+import boto3
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+SNS_TOPIC_NAME = "iam-config-change-monitor-topic"
+
+
+def get_sns_topic_arn(name):
+    # If you attempt to create an already existing topic
+    # it will just return an object representing that topic
+    # https://stackoverflow.com/questions/71088521/how-can-i-get-the-sns-topic-arn-using-the-topic-name-to-publish-a-sns-message-in
+    sns_client = boto3.client("sns")
+    topic = sns_client.create_topic(Name=name)
+    return topic["TopicArn"]
+
+
+def notify_admin(topic, event, account):
+    sns_client = boto3.client("sns")
+    response = sns_client.publish(
+        TargetArn=topic,
+        Subject="Alert: IAM Configuration Change Detected",
+        Message=f"""There has been a {event} change in the IAM configuration in the {account} AWS account.
+        Please investigate immediately""",
+    )
+
+    if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+        logger.info(f"SNS notification sent successfully - {response}")
+    else:
+        logger.error(f"Error sending SNS notification - {response}")
+
+    return response
+
+
+def lambda_handler(event, context):
+    try:
+        detail = event["detail"]
+        account = event["account"]
+        event = detail["eventName"]
+
+        logger.info(f"{event} event detected.")
+        topic = get_sns_topic_arn(SNS_TOPIC_NAME)
+        notify_admin(topic=topic, event=event, account=account)
+        logger.info("Notified the admin")
+
+    except Exception as e:
+        logger.error(f"{e}")
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Lambda function executed successfully."),
+    }
